8f7254003a033aad5ec7b7b19cc5d05e1bd9b76f
Daniel Coelho <1danielcoelho@gmail.com>
2024-04-21 16:31:16 -0300

    Rename utils functions to match the less specific names like on the book just to make it easier to implement, as I was using a check() when I should have match()'d

--- src/parser.cpp
+++ src/parser.cpp
@@ -56,7 +56,7 @@ namespace ParserInternal
 			return previous();
 		};
 
-		bool current_matches_type(TokenType type)
+		bool check(TokenType type)
 		{
 			if (is_at_end())
 			{
@@ -66,11 +66,11 @@ namespace ParserInternal
 			return peek().type == type;
 		};
 
-		bool advance_for_token_types(const std::vector<TokenType>& types)
+		bool match(const std::vector<TokenType>& types)
 		{
 			for (TokenType type : types)
 			{
-				if (current_matches_type(type))
+				if (check(type))
 				{
 					advance();
 					return true;
@@ -86,9 +86,9 @@ namespace ParserInternal
 			return ParseError();
 		}
 
-		std::optional<const Token*> advance_for_token_type_checked(TokenType type, const std::string& error_message)
+		std::optional<const Token*> consume(TokenType type, const std::string& error_message)
 		{
-			if (current_matches_type(type))
+			if (check(type))
 			{
 				return {&advance()};
 			}
@@ -133,43 +133,43 @@ namespace ParserInternal
 
 		std::unique_ptr<Expression> parse_primary()
 		{
-			if (advance_for_token_types({TokenType::FALSE}))
+			if (match({TokenType::FALSE}))
 			{
 				std::unique_ptr<LiteralExpression> new_expr = std::make_unique<LiteralExpression>();
 				new_expr->literal = false;
 				return new_expr;
 			}
 
-			if (advance_for_token_types({TokenType::TRUE}))
+			if (match({TokenType::TRUE}))
 			{
 				std::unique_ptr<LiteralExpression> new_expr = std::make_unique<LiteralExpression>();
 				new_expr->literal = true;
 				return new_expr;
 			}
 
-			if (advance_for_token_types({TokenType::NIL}))
+			if (match({TokenType::NIL}))
 			{
 				std::unique_ptr<LiteralExpression> new_expr = std::make_unique<LiteralExpression>();
 				new_expr->literal = nullptr;
 				return new_expr;
 			}
 
-			if (advance_for_token_types({TokenType::NUMBER, TokenType::STRING}))
+			if (match({TokenType::NUMBER, TokenType::STRING}))
 			{
 				std::unique_ptr<LiteralExpression> new_expr = std::make_unique<LiteralExpression>();
 				new_expr->literal = previous().literal;
 				return new_expr;
 			}
 
-			if (advance_for_token_types({TokenType::LEFT_PAREN}))
+			if (match({TokenType::LEFT_PAREN}))
 			{
 				std::unique_ptr<GroupingExpression> new_expr = std::make_unique<GroupingExpression>();
 				new_expr->expr = parse_expression();
-				advance_for_token_type_checked(TokenType::RIGHT_PAREN, "Expected ')' after expression.");
+				consume(TokenType::RIGHT_PAREN, "Expected ')' after expression.");
 				return new_expr;
 			}
 
-			if (advance_for_token_types({TokenType::IDENTIFIER}))
+			if (match({TokenType::IDENTIFIER}))
 			{
 				std::unique_ptr<VariableExpression> new_expr = std::make_unique<VariableExpression>();
 				new_expr->name = previous();
@@ -182,7 +182,7 @@ namespace ParserInternal
 		std::unique_ptr<Expression> finish_parsing_call(std::unique_ptr<Expression>&& callee)
 		{
 			std::vector<std::unique_ptr<Expression>> arguments;
-			if (!current_matches_type(TokenType::RIGHT_PAREN))
+			if (!check(TokenType::RIGHT_PAREN))
 			{
 				do
 				{
@@ -192,10 +192,10 @@ namespace ParserInternal
 					}
 
 					arguments.push_back(parse_expression());
-				} while (advance_for_token_types({TokenType::COMMA}));
+				} while (match({TokenType::COMMA}));
 			}
 
-			const Token* paren = advance_for_token_type_checked(TokenType::RIGHT_PAREN, "Expected ')' after arguments").value();
+			const Token* paren = consume(TokenType::RIGHT_PAREN, "Expected ')' after arguments").value();
 
 			std::unique_ptr<CallExpression> new_expr = std::make_unique<CallExpression>();
 			new_expr->callee = std::move(callee);
@@ -210,7 +210,7 @@ namespace ParserInternal
 
 			while (true)
 			{
-				if (advance_for_token_types({TokenType::LEFT_PAREN}))
+				if (match({TokenType::LEFT_PAREN}))
 				{
 					expr = finish_parsing_call(std::move(expr));
 				}
@@ -225,7 +225,7 @@ namespace ParserInternal
 
 		std::unique_ptr<Expression> parse_unary()
 		{
-			if (advance_for_token_types({TokenType::BANG, TokenType::MINUS}))
+			if (match({TokenType::BANG, TokenType::MINUS}))
 			{
 				std::unique_ptr<UnaryExpression> new_expr = std::make_unique<UnaryExpression>();
 				new_expr->op = previous();
@@ -240,7 +240,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_unary();
 
-			while (advance_for_token_types({TokenType::SLASH, TokenType::STAR}))
+			while (match({TokenType::SLASH, TokenType::STAR}))
 			{
 				std::unique_ptr<BinaryExpression> new_expr = std::make_unique<BinaryExpression>();
 				new_expr->left = std::move(expr);
@@ -257,7 +257,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_factor();
 
-			while (advance_for_token_types({TokenType::MINUS, TokenType::PLUS}))
+			while (match({TokenType::MINUS, TokenType::PLUS}))
 			{
 				std::unique_ptr<BinaryExpression> new_expr = std::make_unique<BinaryExpression>();
 				new_expr->left = std::move(expr);
@@ -274,7 +274,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_term();
 
-			while (advance_for_token_types({TokenType::GREATER, TokenType::GREATER_EQUAL, TokenType::LESS, TokenType::LESS_EQUAL}))
+			while (match({TokenType::GREATER, TokenType::GREATER_EQUAL, TokenType::LESS, TokenType::LESS_EQUAL}))
 			{
 				std::unique_ptr<BinaryExpression> new_expr = std::make_unique<BinaryExpression>();
 				new_expr->left = std::move(expr);
@@ -291,7 +291,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_comparison();
 
-			while (advance_for_token_types({TokenType::BANG_EQUAL, TokenType::EQUAL_EQUAL}))
+			while (match({TokenType::BANG_EQUAL, TokenType::EQUAL_EQUAL}))
 			{
 				std::unique_ptr<BinaryExpression> new_expr = std::make_unique<BinaryExpression>();
 				new_expr->left = std::move(expr);
@@ -308,7 +308,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_equality();
 
-			while (advance_for_token_types({TokenType::AND}))
+			while (match({TokenType::AND}))
 			{
 				std::unique_ptr<LogicalExpression> new_expr = std::make_unique<LogicalExpression>();
 				new_expr->left = std::move(expr);
@@ -325,7 +325,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_and();
 
-			while (advance_for_token_types({TokenType::OR}))
+			while (match({TokenType::OR}))
 			{
 				std::unique_ptr<LogicalExpression> new_expr = std::make_unique<LogicalExpression>();
 				new_expr->left = std::move(expr);
@@ -342,7 +342,7 @@ namespace ParserInternal
 		{
 			std::unique_ptr<Expression> expr = parse_or();
 
-			if (advance_for_token_types({TokenType::EQUAL}))
+			if (match({TokenType::EQUAL}))
 			{
 				const Token& target = previous();
 				std::unique_ptr<Expression> value = parse_assignment();
@@ -370,7 +370,7 @@ namespace ParserInternal
 		std::unique_ptr<Statement> parse_print_statement()
 		{
 			std::unique_ptr<Expression> expr = parse_expression();
-			advance_for_token_type_checked(TokenType::SEMICOLON, "Expected a ';' after value.");
+			consume(TokenType::SEMICOLON, "Expected a ';' after value.");
 
 			std::unique_ptr<PrintStatement> statement = std::make_unique<PrintStatement>();
 			statement->expression = std::move(expr);
@@ -380,7 +380,7 @@ namespace ParserInternal
 		std::unique_ptr<Statement> parse_expression_statement()
 		{
 			std::unique_ptr<Expression> expr = parse_expression();
-			advance_for_token_type_checked(TokenType::SEMICOLON, "Expected a ';' after expression.");
+			consume(TokenType::SEMICOLON, "Expected a ';' after expression.");
 
 			std::unique_ptr<ExpressionStatement> statement = std::make_unique<ExpressionStatement>();
 			statement->expression = std::move(expr);
@@ -391,25 +391,25 @@ namespace ParserInternal
 		{
 			std::vector<std::unique_ptr<Statement>> statements;
 
-			while (!current_matches_type(TokenType::RIGHT_BRACE) && !is_at_end())
+			while (!check(TokenType::RIGHT_BRACE) && !is_at_end())
 			{
 				statements.push_back(parse_declaration());
 			}
 
-			advance_for_token_type_checked(TokenType::RIGHT_BRACE, "Expected a '}' after block.");
+			consume(TokenType::RIGHT_BRACE, "Expected a '}' after block.");
 			return statements;
 		}
 
 		std::unique_ptr<Statement> parse_if_statement()
 		{
-			advance_for_token_type_checked(TokenType::LEFT_PAREN, "Expected a '(' after 'if'");
+			consume(TokenType::LEFT_PAREN, "Expected a '(' after 'if'");
 			std::unique_ptr<Expression> condition = parse_expression();
-			advance_for_token_type_checked(TokenType::RIGHT_PAREN, "Expected a ')' after 'if' condition");
+			consume(TokenType::RIGHT_PAREN, "Expected a ')' after 'if' condition");
 
 			std::unique_ptr<Statement> then_branch = parse_statement();
 
 			std::unique_ptr<Statement> else_branch = nullptr;
-			if (advance_for_token_types({TokenType::ELSE}))
+			if (match({TokenType::ELSE}))
 			{
 				else_branch = parse_statement();
 			}
@@ -423,9 +423,9 @@ namespace ParserInternal
 
 		std::unique_ptr<Statement> parse_while_statement()
 		{
-			advance_for_token_type_checked(TokenType::LEFT_PAREN, "Expected a '(' after 'while'");
+			consume(TokenType::LEFT_PAREN, "Expected a '(' after 'while'");
 			std::unique_ptr<Expression> condition = parse_expression();
-			advance_for_token_type_checked(TokenType::RIGHT_PAREN, "Expected a ')' after 'while' condition");
+			consume(TokenType::RIGHT_PAREN, "Expected a ')' after 'while' condition");
 
 			std::unique_ptr<Statement> body = parse_statement();
 
@@ -437,15 +437,15 @@ namespace ParserInternal
 
 		std::unique_ptr<Statement> parse_for_statement()
 		{
-			advance_for_token_type_checked(TokenType::LEFT_PAREN, "Expected a '(' after 'for'");
+			consume(TokenType::LEFT_PAREN, "Expected a '(' after 'for'");
 
 			// Parse initializer
 			std::unique_ptr<Statement> initializer;
-			if (advance_for_token_types({TokenType::SEMICOLON}))
+			if (match({TokenType::SEMICOLON}))
 			{
 				initializer = nullptr;
 			}
-			else if (advance_for_token_types({TokenType::VAR}))
+			else if (match({TokenType::VAR}))
 			{
 				initializer = parse_var_declaration();
 			}
@@ -456,19 +456,19 @@ namespace ParserInternal
 
 			// Parse condition
 			std::unique_ptr<Expression> condition = nullptr;
-			if (!current_matches_type(TokenType::SEMICOLON))
+			if (!check(TokenType::SEMICOLON))
 			{
 				condition = parse_expression();
 			}
-			advance_for_token_type_checked(TokenType::SEMICOLON, "Expected a ';' after 'for' condition");
+			consume(TokenType::SEMICOLON, "Expected a ';' after 'for' condition");
 
 			// Parse increment
 			std::unique_ptr<Expression> increment = nullptr;
-			if (!current_matches_type(TokenType::RIGHT_PAREN))
+			if (!check(TokenType::RIGHT_PAREN))
 			{
 				increment = parse_expression();
 			}
-			advance_for_token_type_checked(TokenType::RIGHT_PAREN, "Expected a ')' after 'for' loop clauses");
+			consume(TokenType::RIGHT_PAREN, "Expected a ')' after 'for' loop clauses");
 
 			// Parse body
 			std::unique_ptr<Statement> body = parse_statement();
@@ -516,23 +516,23 @@ namespace ParserInternal
 
 		std::unique_ptr<Statement> parse_statement()
 		{
-			if (advance_for_token_types({TokenType::FOR}))
+			if (match({TokenType::FOR}))
 			{
 				return parse_for_statement();
 			}
-			if (advance_for_token_types({TokenType::IF}))
+			if (match({TokenType::IF}))
 			{
 				return parse_if_statement();
 			}
-			if (advance_for_token_types({TokenType::PRINT}))
+			if (match({TokenType::PRINT}))
 			{
 				return parse_print_statement();
 			}
-			if (advance_for_token_types({TokenType::WHILE}))
+			if (match({TokenType::WHILE}))
 			{
 				return parse_while_statement();
 			}
-			if (advance_for_token_types({TokenType::LEFT_BRACE}))
+			if (match({TokenType::LEFT_BRACE}))
 			{
 				std::unique_ptr<BlockStatement> block_statement = std::make_unique<BlockStatement>();
 				block_statement->statements = parse_block();
@@ -544,15 +544,15 @@ namespace ParserInternal
 
 		std::unique_ptr<Statement> parse_var_declaration()
 		{
-			std::optional<const Token*> name = advance_for_token_type_checked(TokenType::IDENTIFIER, "Expected a variable name");
+			std::optional<const Token*> name = consume(TokenType::IDENTIFIER, "Expected a variable name");
 
 			std::unique_ptr<Expression> initializer;
-			if (advance_for_token_types({TokenType::EQUAL}))
+			if (match({TokenType::EQUAL}))
 			{
 				initializer = parse_expression();
 			}
 
-			advance_for_token_type_checked(TokenType::SEMICOLON, "Expected a ';' after variable declaration");
+			consume(TokenType::SEMICOLON, "Expected a ';' after variable declaration");
 
 			std::unique_ptr<VariableDeclarationStatement> statement = std::make_unique<VariableDeclarationStatement>();
 			statement->name = *name.value();
@@ -563,11 +563,11 @@ namespace ParserInternal
 		// TODO: Not use string comparison...
 		std::unique_ptr<FunctionStatement> parse_function_declaration(const std::string& kind)
 		{
-			const Token* name = advance_for_token_type_checked(TokenType::IDENTIFIER, "Expected " + kind + " name").value();
-			advance_for_token_type_checked(TokenType::LEFT_PAREN, "Expected '(' after " + kind + " name");
+			const Token* name = consume(TokenType::IDENTIFIER, "Expected " + kind + " name").value();
+			consume(TokenType::LEFT_PAREN, "Expected '(' after " + kind + " name");
 
 			std::vector<Token> parameters;
-			if (!current_matches_type(TokenType::RIGHT_PAREN))
+			if (!check(TokenType::RIGHT_PAREN))
 			{
 				do
 				{
@@ -576,12 +576,12 @@ namespace ParserInternal
 						Lox::report_error(peek(), "Can't have more than 255 function parameters");
 					}
 
-					parameters.push_back(*advance_for_token_type_checked(TokenType::IDENTIFIER, "Expected parameter name").value());
-				} while (current_matches_type(TokenType::COMMA));
+					parameters.push_back(*consume(TokenType::IDENTIFIER, "Expected parameter name").value());
+				} while (match({TokenType::COMMA}));
 			}
 
-			advance_for_token_type_checked(TokenType::RIGHT_PAREN, "Expected ')' after function parameters");
-			advance_for_token_type_checked(TokenType::LEFT_BRACE, "Expected '{' before " + kind + " body");
+			consume(TokenType::RIGHT_PAREN, "Expected ')' after function parameters");
+			consume(TokenType::LEFT_BRACE, "Expected '{' before " + kind + " body");
 			std::vector<std::unique_ptr<Statement>> body = parse_block();
 
 			std::unique_ptr<FunctionStatement> statement = std::make_unique<FunctionStatement>();
@@ -595,11 +595,11 @@ namespace ParserInternal
 		{
 			try
 			{
-				if (advance_for_token_types({TokenType::FUN}))
+				if (match({TokenType::FUN}))
 				{
 					return parse_function_declaration("function");
 				}
-				if (advance_for_token_types({TokenType::VAR}))
+				if (match({TokenType::VAR}))
 				{
 					return parse_var_declaration();
 				}
